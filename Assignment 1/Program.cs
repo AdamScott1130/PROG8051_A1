51902c808d64551562e80fa50d7c69ddd3e7570f
AdamScott1130 <11adamscott30@gmail.com>
2025-09-10 18:38:49 -0400

    - Added Pet Creation feature - Added reusable subroutine for validating user input.

--- Assignment 1/Program.cs
+++ Assignment 1/Program.cs
@@ -1,4 +1,51 @@
 ﻿// This program implemenets a virtual pet simulator
+class petSim
+{
+    static void Main()
+    {
+        // Validates user input is an integer and returns that integer
+        static int getIntInput(int numOptions)
+        {
+            bool validInput = false;
+            int userInput = -1;
+            while (validInput == false)
+            {
+                try
+                {
+                    userInput = int.Parse(Console.ReadLine());
+                    if (userInput < 1 || userInput > numOptions)
+                    {
+                        Console.WriteLine("Enter a valid input!");
+                    }
+                    else
+                    {
+                        validInput = true; // break loop
+                    }
+                }
+                catch
+                {
+                    Console.WriteLine("Enter a valid input!");
+                }
+            }
+            return userInput;
+        }
 
-// Pet Creation
+        float hunger = 5;
+        float happiness = 5;
+        float health = 5;
+
+        // Program Requirement 1 - Pet Creation
+        string[] petTypes = ["Dog", "Cat", "Rabbit"];
+        int userInput = -1;
+        int numPetTypes = petTypes.Length;
+        Console.WriteLine("Choose your type of pet: \n 1 - Dog \n 2 - Cat\n 3 - Rabbit");
+        int userPetType = getIntInput(numPetTypes);
+        Console.WriteLine("You have chosen {0}! Please enter a name for your {0}:", petTypes[userPetType - 1], petTypes[userPetType - 1]);
+        string userPetName = Console.ReadLine();
+        Console.WriteLine("Welcome {0}!", userPetName);
+
+
+    }
+}
+
 
